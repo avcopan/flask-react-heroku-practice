d272adbae4675c2e05ad44af0ae52418e7db6236
Andreas Copan <avcopan@gmail.com>
2023-08-29 13:54:34 -0400

    Seems to work fine

--- server/api.py
+++ server/api.py
@@ -1,10 +1,14 @@
 import time
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="../dist", static_url_path="/")
 
 
-@app.route('/api/time')
+@app.route("/")
+def index():
+    return app.send_static_file("index.html")
+
+
+@app.route("/api/time")
 def get_current_time():
-    return {'content': time.time()}
-
+    return {"content": time.time()}
