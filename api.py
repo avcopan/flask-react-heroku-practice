100a7dce1510c5b80179500861b2c95f8f8c7eac
Andreas Copan <avcopan@gmail.com>
2023-08-29 15:31:32 -0400

    Setting up for Heroku deployment

--- api.py
+++ api.py
@@ -1,9 +1,20 @@
 import time
 from flask import Flask
+from flask.helpers import send_from_directory
+from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='client/dist', static_url_path='')
+CORS(app)
 
 
 @app.route('/api/time')
+@cross_origin()
 def get_current_time():
     return {'content': time.time()}
+
+
+
+@app.route('/')
+@cross_origin()
+def server():
+    return send_from_directory(app.static_folder, "index.html")
